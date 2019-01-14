9f32b2ef0d45f73dfc069ffdcf4987150d72e0bd
akingyin1987 <akingyin@163.com>
2019-01-14 17:05:37 +0800

    ffffff

--- app/src/main/java/com/akingyin/androidjetpackdemo/ui/main/MainViewModel.kt
+++ app/src/main/java/com/akingyin/androidjetpackdemo/ui/main/MainViewModel.kt
@@ -1,16 +1,25 @@
 package com.akingyin.androidjetpackdemo.ui.main
 
+import android.app.Application
 import android.view.View
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
 import com.akingyin.androidjetpackdemo.entity.User
-import com.blankj.utilcode.util.TimeUtils
+import com.akingyin.androidjetpackdemo.ui.adapter.UserListAdapter
+import com.chad.library.adapter.base.BaseQuickAdapter
 import java.util.*
 
 
-class MainViewModel : ViewModel() ,View.OnClickListener {
+
+
+class MainViewModel(application: Application) : AndroidViewModel(application) ,View.OnClickListener ,BaseQuickAdapter.OnItemClickListener
+ ,BaseQuickAdapter.OnItemLongClickListener{
     // TODO: Implement the ViewModel
     var  user:User = User()
 
+    var  adapter:UserListAdapter? = null
+     val mUsers = MutableLiveData<User>()
+    var users:MutableList<User> = mutableListOf()
      init {
          user.age = 1
          user.createDay = Date()
@@ -21,6 +30,24 @@ class MainViewModel : ViewModel() ,View.OnClickListener {
         println("onClick")
         var  random:Random = Random()
         user.name = random.nextInt(100).toString()+"name"
+        var  user2 = User()
+        user2.name = user.name
+        user2.createDay = Date()
+        users.add(0,user2)
+        println("adapter= ${adapter?.itemCount}")
+        adapter?.notifyDataSetChanged()
+
 
     }
+
+    override fun onItemClick(adapter2: BaseQuickAdapter<*, *>?, view: View?, position: Int) {
+        println("itemClick=${ adapter?.getItem(position)?.name}")
+    }
+
+    override fun onItemLongClick(adapter2: BaseQuickAdapter<*, *>?, view: View?, position: Int): Boolean {
+        adapter2?.remove(position)
+       return true
+    }
+
+    
 }
